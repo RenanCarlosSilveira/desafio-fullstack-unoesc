e1c41fc29e7016a7c87cf0e0a81834b5e92fe24e
RenanCarlosSilveira <71138883+RenanCarlosSilveira@users.noreply.github.com>
2022-03-26 01:39:35 -0300

    Commit Inicial
    Controller de inicialização do app
    Importação de estilos CSS
    Inclusão de dependências webjars e thymeleaf
    Criação e ajuste dos primeiros templates

---
+++ src/main/java/br/edu/unoesc/desafiofullstackunoesc/controller/FuncionarioController.java
@@ -0,0 +1,10 @@
+package br.edu.unoesc.desafiofullstackunoesc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/funcionarios")
+public class FuncionarioController {
+
+}
---
+++ src/main/java/br/edu/unoesc/desafiofullstackunoesc/controller/MainController.java
@@ -0,0 +1,12 @@
+package br.edu.unoesc.desafiofullstackunoesc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("")
+	public String home() {
+		return "/home";
+	}
+}
